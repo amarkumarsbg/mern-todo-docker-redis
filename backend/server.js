ff3d6ad89c38661d4c103c475ccc148803f194a7
Amar Kumar <amarkumarsbg13@gmail.com>
2024-12-29 15:22:54 +0530

    Redis Implementation

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,15 @@ import userRouter from "./routes/userRoute.js";
 import todoRouter from "./routes/todoRoute.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { Redis } from "ioredis";
+import axios from "axios";
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+const redisClient = new Redis();
 
 const port = process.env.PORT || 5000;
 
@@ -23,6 +27,27 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+//Redis implementation
+app.get("/posts", async (req, res) => {
+  try {
+    const cachedData = await redisClient.get("posts");
+    if (cachedData !== null) {
+      console.log("cached data");
+      return res.json(JSON.parse(cachedData));
+    } else {
+      console.log("Not cached data");
+      const { data } = await axios(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      await redisClient.setex("posts", 20, JSON.stringify(data));
+
+      return res.json(data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/todo", todoRouter);
